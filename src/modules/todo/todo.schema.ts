d8f7df10a0cf07c53ef9b04ef555596065de03ae
Viacheslav Dmitriiev <dmitriievv@mydigicode.com>
2021-01-20 18:13:57 +0200

    added todo schema

---
+++ src/modules/todo/todo.schema.ts
@@ -0,0 +1,12 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TodoDocument = Todo & Document;
+
+@Schema({ timestamps: true })
+export class Todo {
+  @Prop({ required: true, minlength: 1, maxlength: 200 })
+  text: string;
+}
+
+export const TodoSchema = SchemaFactory.createForClass(Todo);
