04a642bb320270056c992ad32dd9fc38d803cbae
Viacheslav Dmitriiev <dmitriievv@mydigicode.com>
2021-02-22 17:28:01 +0200

    extended swagger doc on Todo controller

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -47,16 +47,20 @@ export class TodoController {
     return this.todoService.getTodos();
   }
 
+  @ApiOperation({ description: 'Update many todo items at one transaction' })
   @Patch('updateMany')
   updateMany(@Body() updateData: UpdateManyDto) {
     return this.todoService.updateMany(updateData);
   }
 
+  @ApiOperation({ description: 'Delete many todo items at one transaction' })
   @Delete('deleteMany')
+  @HttpCode(204)
   deleteMany(@Body() { ids }: DeleteManyDto) {
     return this.todoService.deleteMany(ids);
   }
 
+  @ApiOkResponse({ type: TodoDto })
   @Get(':id')
   getTodo(@Param('id') id: string) {
     return this.todoService.getTodo(id);
